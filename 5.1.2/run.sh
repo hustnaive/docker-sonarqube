3439c249ff1bf14eb46c6bcd9dc970042e96d83b
Evgeny Mandrikov <mandrikov@gmail.com>
2015-08-16 00:40:55 +0200

    (wip)

--- 5.1.2/run.sh
+++ 5.1.2/run.sh
@@ -5,7 +5,7 @@ if [ "${1:0:1}" != '-' ]; then
 	exec "$@"
 fi
 
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+exec java -jar -Xmx3m lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
